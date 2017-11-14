29e68b0ee86825ed32c3e92700ae34cc1065b52b
Ernesto Acevedo <eacevedo@debian.cl>
2017-11-14 14:15:31 -0300

    Fix linter issue

--- src/InputTag.vue
+++ src/InputTag.vue
@@ -1,5 +1,5 @@
 <script>
-  /*eslint-disable*/
+  /* eslint-disable */
   const validators = {
     email: new RegExp(/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/),
     url : new RegExp(/^(https?|ftp|rmtp|mms):\/\/(([A-Z0-9][A-Z0-9_-]*)(\.[A-Z0-9][A-Z0-9_-]*)+)(:(\d+))?\/?/i),
@@ -7,7 +7,7 @@
     digits : new RegExp(/^[\d() \.\:\-\+#]+$/),
     isodate : new RegExp(/^\d{4}[\/\-](0?[1-9]|1[012])[\/\-](0?[1-9]|[12][0-9]|3[01])$/)
   }
-  /*eslint-enable*/
+  /* eslint-enable */
 
   export default {
     name: 'InputTag',
